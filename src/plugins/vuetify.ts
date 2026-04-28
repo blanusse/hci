0fe5daf47c4374994141471c4e8aefb5617fe844
Beltran Lanusse <blanusse@itba.edu.ar>
2026-04-27 22:35:54 -0300

    d

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,21 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Composables
+import { createVuetify } from 'vuetify'
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+
+import 'vuetify/styles'
+import colors from 'vuetify/util/colors'
+
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    defaultTheme: 'system',
+  },
+})
